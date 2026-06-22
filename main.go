b4b851ee71176b63c6cb638b756edfca2646df49
Boidushya <boidushyabhattacharya@gmail.com>
2026-06-22 19:45:34 +0530

    feat: allow composer.betterlyrics.org origin

--- main.go
+++ main.go
@@ -120,7 +120,7 @@ func main() {
 	}
 
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"https://music.youtube.com", "http://localhost:*", "https://lyrics-api-docs.boidu.dev", "https://braccato.boidu.dev", "https://composer.boidu.dev"},
+		AllowedOrigins:   []string{"https://music.youtube.com", "http://localhost:*", "https://lyrics-api-docs.boidu.dev", "https://braccato.boidu.dev", "https://composer.boidu.dev", "https://composer.betterlyrics.org"},
 		AllowCredentials: true,
 	})
 
